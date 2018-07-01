d2ee97ebdd9203dc9814d32231f7bc6902c1ea45
Fazzani <tunisienheni@outlook.com>
2018-07-01 23:04:21 +0200

    fix

--- Synker.Scheduled.Example/HostedServices/QuoteOfTheDayTask.cs
+++ Synker.Scheduled.Example/HostedServices/QuoteOfTheDayTask.cs
@@ -1,26 +1,39 @@
 ﻿namespace Synker.Scheduled.Example.HostedServices
 {
-    using System.Net.Http;
-    using System.Threading;
-    using System.Threading.Tasks;
+    using Microsoft.Extensions.Logging;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Linq;
     using Synker.Scheduled.HostedServices;
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
     // uses https://theysaidso.com/api/
     public class QuoteOfTheDayTask : IScheduledTask
     {
-        public string Schedule => "* */6 * * *";
-        
+        private readonly ILogger<QuoteOfTheDayTask> _loggger;
+
+        public string Schedule => Environment.GetEnvironmentVariable($"{nameof(QuoteOfTheDayTask)}_ScheduledTask") ?? "* */6 * * *";
+
+
+        public QuoteOfTheDayTask(ILoggerFactory loggerFactory)
+        {
+            _loggger = loggerFactory.CreateLogger<QuoteOfTheDayTask>();
+        }
+
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
-            var httpClient = new HttpClient();
-
-            var quoteJson = JObject.Parse(await httpClient.GetStringAsync("http://quotes.rest/qod.json"));
-
-            QuoteOfTheDay.Current = JsonConvert.DeserializeObject<QuoteOfTheDay>(quoteJson["contents"]["quotes"][0].ToString());
+            using (var httpClient = new HttpClient())
+            {
+                _loggger.LogDebug($"{nameof(QuoteOfTheDayTask)} Executing start");
+                var quoteJson = JObject.Parse(await httpClient.GetStringAsync("http://quotes.rest/qod.json"));
+                QuoteOfTheDay.Current = JsonConvert.DeserializeObject<QuoteOfTheDay>(quoteJson["contents"]["quotes"][0].ToString());
+                _loggger.LogDebug($"{nameof(QuoteOfTheDayTask)} Executing end");
+            }
         }
+
     }
-    
+
     public class QuoteOfTheDay
     {
         public static QuoteOfTheDay Current { get; set; }
@@ -29,7 +42,7 @@
         {
             Current = new QuoteOfTheDay { Quote = "No quote", Author = "Maarten" };
         }
-        
+
         public string Quote { get; set; }
         public string Author { get; set; }
     }
--- Synker.Scheduled.Example/HostedServices/SomeOtherTask.cs
+++ Synker.Scheduled.Example/HostedServices/SomeOtherTask.cs
@@ -1,4 +1,5 @@
 ﻿using Synker.Scheduled.HostedServices;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,7 +7,7 @@ namespace Synker.Scheduled.Example.HostedServices
 {
     public class SomeOtherTask : IScheduledTask
     {
-        public string Schedule => "0 5 * * *";
+        public string Schedule => Environment.GetEnvironmentVariable($"{nameof(SomeOtherTask)}_ScheduledTask") ?? "0 5 * * *";
 
         public async Task ExecuteAsync(CancellationToken cancellationToken)
         {
